d5b41929e2cf900157479c4fc4c2f0e8ab030422
Dragan Belegic <www.belegic@gmail.com>
2023-04-21 16:04:36 +0200

    initial commit

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+interface ButtonProps  {
+  label: string;
+}
+const Button:React.FC<ButtonProps> = ({ label}) => {
+  return (
+    <div>{label}</div>
+  )
+}
+
+export default Button
